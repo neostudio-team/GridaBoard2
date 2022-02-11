9afcc7f1644b149768289386ed13da4d83eea69a
Seo-Wonho <swh1182@neolab.net>
2022-02-11 11:18:45 +0900

    Feat: 파일로 보드 생성 후 저장하기(덮어쓰기) 시, 보드명 선입력

--- src/GridaBoard/Save/PdfDialogTextArea.tsx
+++ src/GridaBoard/Save/PdfDialogTextArea.tsx
@@ -1,6 +1,7 @@
 import { DialogContent, makeStyles, TextField } from '@material-ui/core';
 import { LensTwoTone } from '@material-ui/icons';
 import React, { useState } from 'react';
+import { store } from '../client/pages/GridaBoard';
 import getText from "../language/language";
 
 
@@ -23,11 +24,18 @@ const PdfDialogTextArea = (props: Props) => {
   const {saveType, ...rest} = props;
   const classes = useStyles();
   let textRef = null as HTMLElement;
-  const [pdfName, setPdfName] = useState('');
+  const isNewDoc = store.getState().docConfig.isNewDoc;
+  let isNewLoadFileName = "";
+  if(isNewDoc && saveType == "overwrite"){
+    if(store.getState().docConfig.docName !== "undefined"){
+      isNewLoadFileName = store.getState().docConfig.docName;
+    }
+  }
+  const [pdfName, setPdfName] = useState(isNewLoadFileName);
 
   const onChange = (e) => {
     let pdfName = e.target.value;
-    
+
     //이름 설정시 바로 특정 문자만 사용할 수 있도록 수정
     const checkAllow = pdfName.match(/[^a-zA-Z0-9가-힇ㄱ-ㅎㅏ-ㅣぁ-ゔァ-ヴー々〆〤一-龥0-9.+_\-.]/g);
     if(checkAllow !== null){
@@ -37,9 +45,9 @@ const PdfDialogTextArea = (props: Props) => {
     }
 
     setPdfName(pdfName);
-    props.onTextAreaChange(pdfName);
   };
-
+  
+  props.onTextAreaChange(pdfName);
 
   return (
     <DialogContent>
