fa2360eb58e40a52dda35050f16297611e51e7f2
Seo-Wonho <swh1182@neolab.net>
2022-02-17 17:10:34 +0900

    Feat: 보드 이동 dialog 상 Unshelved 표시 수정

--- src/boardList/layout/component/dialog/detail/MoveDialog.tsx
+++ src/boardList/layout/component/dialog/detail/MoveDialog.tsx
@@ -324,7 +324,7 @@ const MoveDialog = (props: Props)=>{
       {categoryData.map((el,idx)=>{
         return (<div key={idx} onClick={e=>selectCategory(idx)} className={`${moveClasses.contentItems} ${idx === nowSelect ? moveClasses.selectedItem: ""}`}>
             <div>
-              {el[0]}{` (${el[2]})`}
+              {el[0] === "Unshelved" ? getText("boardList_unshelved").substring(0,5) : el[0]}{` (${el[2]})`}
             </div>
             { nowSelect === idx ? 
             <SvgIcon>
